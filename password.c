34074f808624f0fa0ac478dbc7209c53048a5880
Firepeashooter1 <firepeashooter1@gmail.com>
2025-08-28 18:57:18 +0000

    fixed code

---
+++ password.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main() {
+	char  password[];
+	int nukes = 3;
+
+	printf("Enter an integer: ");
+
+	while (nukes != 0) {
+		if (scanf("%d", &password) != "1234") {
+			printf("Incorrect, Sending Nukes");
+			nukes = nukes - 1;
+			return 0;
+		} else {
+			printf("Your in");
+			return 0;
+		}
+	}
+	return 0;
+}
